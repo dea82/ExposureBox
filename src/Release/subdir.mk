390f91e540784072e85ad8b2dfe6f1498327699f
Andreas <andreas.laghamn@gmail.com>
2015-10-26 22:10:56 +0100

    Changed makefile script.

--- src/Release/subdir.mk
+++ src/Release/subdir.mk
@@ -3,6 +3,24 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../butt.s \
+../buzz.s \
+../colo.s \
+../dast.s \
+../eepr.s \
+../lcdc.s \
+../ledc.s \
+../lise.s \
+../main.s \
+../menu.s \
+../mstmIface.s \
+../pomm.s \
+../sche.s \
+../seri.s \
+../spii.s \
+../stmt.s 
+
 C_SRCS += \
 ../butt.c \
 ../buzz.c \
@@ -51,6 +69,24 @@ OBJS += \
 ./such.o \
 ./util.o 
 
+S_DEPS += \
+./butt.d \
+./buzz.d \
+./colo.d \
+./dast.d \
+./eepr.d \
+./lcdc.d \
+./ledc.d \
+./lise.d \
+./main.d \
+./menu.d \
+./mstmIface.d \
+./pomm.d \
+./sche.d \
+./seri.d \
+./spii.d \
+./stmt.d 
+
 C_DEPS += \
 ./butt.d \
 ./buzz.d \
@@ -81,6 +117,13 @@ C_DEPS += \
 	@echo 'Building file: $<'
 	@echo 'Invoking: AVR Compiler'
 	avr-gcc -Wall -g2 -gstabs -Os -fpack-struct -fshort-enums -ffunction-sections -fdata-sections -std=gnu99 -funsigned-char -funsigned-bitfields -mmcu=atmega32 -DF_CPU=16000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+%.o: ../%.s
+	@echo 'Building file: $<'
+	@echo 'Invoking: AVR Assembler'
+	avr-gcc -x assembler-with-cpp -g2 -gstabs -mmcu=atmega32 -DF_CPU=16000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -c -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 

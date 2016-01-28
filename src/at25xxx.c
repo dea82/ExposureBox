520a8938522ab276468990f2419675a969220864
Andreas <andreas.laghamn@gmail.com>
2016-01-28 21:13:00 +0100

    no message

--- src/at25xxx.c
+++ src/at25xxx.c
@@ -5,4 +5,44 @@
  *      Author: andreas
  */
 
+#include "at25xxx.h"
 
+static uint8_t ready_U08(void);
+
+uint8_t write_U08(uint8_t *source_U08, uint16_t address_U16, uint8_t bytes_U08)
+{
+    uint8_t ret_U08 = 1;
+
+    if (address_U16 + bytes_U08 <= MAX_ADDRESS)
+    {
+        /* Write memory page (max 64 bytes) */
+        while (bytes_U08 > 0)
+        {
+            /* TODO: Chip select */
+            if (ready_U08)
+            {
+
+            }
+            else
+            {
+                /* Failed to communicate or busy too long. */
+                return 0;
+            }
+        }
+    }
+    else
+    {
+        /* Memory address out of range. */
+        return 0;
+    }
+
+    return ret_U08;
+}
+
+static uint8_t ready_U08(void)
+{
+    uint8_t ret_U08 = 1;
+    return ret_U08;
+}
+
+static uint8_t chipSelectH(uint8_t )
--- src/at25xxx.h
+++ src/at25xxx.h
@@ -8,6 +8,13 @@
 #ifndef AT25XXX_H_
 #define AT25XXX_H_
 
+#if(DEVICE == AT25256)
+#define MAX_ADDRESS AT25256_MAX_ADDRESS
+#elif(DEVICE == AT25128)
+#define MAX_ADDRESS
+#endif
 
+#define AT25256_MAX_ADDRESS 32767
+#define AT25128_MAX_ADDRESS 16383
 
 #endif /* AT25XXX_H_ */
